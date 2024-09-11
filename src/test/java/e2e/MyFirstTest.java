b21c4044f7a3759c83027b3fb4994a263bb7e55c
saiotest <elyermad@gmail.com>
2024-09-10 23:15:36 -0300

    init project with selenium java + JUnit5 + Maven

---
+++ src/test/java/e2e/MyFirstTest.java
@@ -0,0 +1,38 @@
+package e2e;
+
+import java.time.Duration;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyFirstTest {
+
+    @Test
+    public void TC1LoginTest(){
+        ChromeDriver web = new ChromeDriver();
+        web.manage().window().maximize();
+        web.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        web.get("https://saucedemo.com");
+
+        WebElement usernameInput = web.findElement(By.cssSelector("input[data-test='username']"));
+        usernameInput.sendKeys("standard_user");
+
+        WebElement passwordInput = web.findElement(By.cssSelector("input[data-test='password']"));
+        passwordInput.sendKeys("secret_sauce");
+
+        WebElement loginButton = web.findElement(By.id("login-button"));
+        loginButton.click();
+
+        WebElement productList = web.findElement(By.cssSelector("[data-test='inventory-list']"));
+        Boolean isProductListVisible = productList.isDisplayed();
+
+        Assertions.assertTrue(isProductListVisible);
+
+        web.quit();
+        System.out.println("🧪 TEST PASSED: TC1 Login Test");
+    }
+}
